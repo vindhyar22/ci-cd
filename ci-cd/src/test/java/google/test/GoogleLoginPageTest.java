907e0ba4446537c492771ffab1d33bcf19be54e7
LAPTOP-NNQF56CK\91914 <vindhya002@gmail.com>
2023-08-09 13:23:37 +0530

    test

--- ci-cd/src/test/java/google/test/GoogleLoginPageTest.java
+++ ci-cd/src/test/java/google/test/GoogleLoginPageTest.java
@@ -28,7 +28,7 @@ public class GoogleLoginPageTest {
 	private WebDriver driver;
 	private ExtentReports extent;
 	private ExtentTest test;
-	//commit- tested
+	//commit- test
 	
 	@BeforeTest
 	public void setUp() {
